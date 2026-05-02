b5aa4d4da7cb61ac2a2a1a21c1d9b693e511fec4
HRSH455 <bommiharsha7@gmail.com>
2026-05-02 17:45:12 +0530

    feat:springVer Downgrade for cloudCompatibility

--- product-service/src/test/java/com/product_service/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/product_service/product/ProductServiceApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.mongodb.MongoDBContainer;
+import org.testcontainers.containers.MongoDBContainer;
 
 import static io.restassured.RestAssured.*;
 
